1755a11121b7f0a6954a53eee5c83ff42d4cef43
marvanessa1 <marvanessa1@live.com>
2022-05-07 20:24:21 -0700

    set up Reaction schema, in Thought model

--- models/Thought.js
+++ models/Thought.js
@@ -3,12 +3,42 @@ const { Schema, model } = require('mongoose');
 const { stringify } = require('querystring');
 const dateFormat = require('../utils/dateFormat');
 
+const reactionSchema = new Schema(
+    {
+        reactionId: {
+            type: Schema.Types.ObjectId(),
+            default: () => new Types.ObjectId(),
+        },
+        reactionBody: {
+            type: String,
+            required: true,
+            minlength: 1,
+            maxlength: [280, 'Maximum character count is 280!'],
+        },
+        username: {
+            type: String,
+            required: true,
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+            get: (timeStamp) => dateFormat(timestamp),
+        },
+    },
+    {
+        toJSON: {
+            getters: true,
+        },
+        id: false,
+    }
+)
+
 const thoughtSchema = new Schema({
     thoughtText: {
         type: String,
         required: true,
         minlength: 1,
-        maxlength: 280,
+        maxlength: [280, 'Maximum character count is 280!'],
     },
     createdAt: {
         type: Date,
