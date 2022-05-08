d2da2e41329eedb39b0bbb5b7468200d9a5c25cb
marvanessa1 <marvanessa1@live.com>
2022-05-07 23:13:38 -0700

    added CRUD user controllers

---
+++ routes/api/thoughtRoutes.js
@@ -0,0 +1,9 @@
+const router = require("express").Router();
+
+//GET to get all thoughts
+
+//GET to get asingle thought by its _id
+
+//POST to create a new thought
+
+module.exports = router
---
+++ routes/api/userRoutes.js
@@ -0,0 +1,13 @@
+const router = require("express").Router();
+
+//GET all users
+
+//GET a single user by its _id and populated thought and friend data
+
+//POST a new user
+
+//PUT to update a user by its _id
+
+//DELETE to remove a user by its _id
+
+module.exports = router
